4cc33ae04c25b18a81b5d5368704bc6398c9fa84
Pai026 <abhirampai1999@gmail.com>
2021-05-28 09:28:03 +0530

    fixes in model

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,13 +1,13 @@
 from ..db import product_collection
-from tensorflow.keras.models import load_model
-from keras.preprocessing import image
+import tflite_runtime.interpreter as tflite
+from PIL import Image
 import urllib.request
 import cv2
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 import numpy as np
-model_file = "app/model/DiseaseDetectionModel.h5"
-severity_file = "app/model/SeverityModel.h5"
+model_file = "app/model/DiseaseDetectionModel.tflite"
+severity_file = "app/model/SeverityModel.tflite"
 
 def product_helper(product) -> dict:
     return {
@@ -20,29 +20,35 @@ def product_helper(product) -> dict:
     }
 
 def get_user_disease_detail(imageUrl):
-    detectionModel = load_model(model_file)
+    detectionModel = tflite.Interpreter(model_file)
     resp = urllib.request.urlopen(imageUrl)
+    input_details = detectionModel.get_input_details()
+    detectionModel.resize_tensor_input(
+    input_details[0]['index'], (1, 64, 64, 3))
+    output_details = detectionModel.get_output_details()
+    detectionModel.allocate_tensors()
     imageUploaded = np.asarray(bytearray(resp.read()), dtype="uint8")
     test_image = cv2.imdecode(imageUploaded, cv2.IMREAD_COLOR)
     resized_shape = (64,64)
     test_img = cv2.resize(test_image,(resized_shape[1],resized_shape[0]))
     skinDiseaseTypes=['blackhead', 'Acne', 'kutil filiform', 'flek hitam', 'folikulitis', 'milia', 'Dermatitis perioral', 'Karsinoma', 'panu', 'melanoma', 'herpes', 'Eksim', 'papula', 'whitehead', 'Tinea facialis', 'rosacea', 'Pustula', 'psoriasis']
-    x = image.img_to_array(test_img)
-    x = np.expand_dims(x, axis = 0)
-
-    x /= 255
-
-    custom = detectionModel.predict(x)
-    x = np.array(x, 'float32')
-    a=custom[0]
-    ind=np.argmax(a)
+    new_img = test_img.astype(np.float32)
+    new_img /=255.0
+    detectionModel.set_tensor(input_details[0]['index'], [new_img])
+    detectionModel.invoke()
+    output_data = detectionModel.get_tensor(output_details[0]['index'])
+    ind=(np.argmax(output_data))
     if(skinDiseaseTypes[ind] == 'Acne'):
-        severityModel = load_model(severity_file)
-        severityLevel=['Level_0', 'Level_2', 'Level_1']
-        custom = severityModel.predict(x)
-        x = np.array(x, 'float32')
-        a=custom[0]
-        index=np.argmax(a)
+        severityModel = tflite.Interpreter(model_path=severity_file)
+        severityModel.resize_tensor_input(
+        input_details[0]['index'], (1, 64, 64, 3))
+        output_details = severityModel.get_output_details()
+        severityModel.allocate_tensors()
+        severityLevel=['Level_0', 'Level_1','Level_2']
+        severityModel.set_tensor(input_details[0]['index'], [new_img])
+        severityModel.invoke()
+        output_data = severityModel.get_tensor(output_details[0]['index'])
+        index=(np.argmax(output_data))
         if(severityLevel[index]=="Level_0"):
             products=product_collection.find({'cures':skinDiseaseTypes[ind]})
             result=[]
